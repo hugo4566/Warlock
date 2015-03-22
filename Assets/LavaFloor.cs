d6534a42fbbe8e6f9bb68d7d7c4a900baa4e27b7
Hugo Prado Figueiredo <hugo4566@gmail.com>
2015-03-22 14:29:52 -0300

    Lava floor,safe floor, player health and damage over time

--- Assets/LavaFloor.cs
+++ Assets/LavaFloor.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class explode : MonoBehaviour {
+public class LavaFloor : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
@@ -10,22 +10,16 @@ public class explode : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		Destroy(gameObject,1.5f);
+	
 	}
-
-	void OnCollisionEnter (Collision collisionInfo){
+		
+	void OnCollisionStay(Collision collisionInfo) {
 		if (collisionInfo.gameObject.name == "Sphere") {
 			PlayerStats ps= collisionInfo.gameObject.GetComponent<PlayerStats>();
-			ps.hp -= 10;
+			ps.hp -= Time.deltaTime;
 			if(ps.hp <=0)
 				Destroy(collisionInfo.gameObject);
 			Debug.Log (ps.namePlayer + "  : " + ps.hp);
-			Vector3 forceVec = collisionInfo.rigidbody.velocity.normalized*(10f/(ps.hp/100));
-			collisionInfo.rigidbody.AddForce(forceVec,ForceMode.Impulse);
-			Destroy(gameObject);
-
-
-
 		}
 	}
 }
---
+++ Assets/PlayerStats.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerStats : MonoBehaviour {
+
+	public string namePlayer { get; set;}
+	public float hp { get; set;} 
+
+	// Use this for initialization
+	void Start () {
+		namePlayer = "Sphere";
+		hp = 100.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
